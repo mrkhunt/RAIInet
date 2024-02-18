1e3a2fdcb3354e87a33a01a5e5501681de8ad776
Mahdi Raza Khunt <mrkhunt@uwaterloo.ca>
2024-02-18 18:35:06 -0500

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  std::cout << "This game was created using C++ as a final project for the CS246 Fall 2023 course offering at the University of Waterloo." << std::endl;
+  std::cout << "This project was made by Mahdi Raza Khunt, Karthik Krishna Viriyala, and Harsha Puranik between November and December 2023." << std::endl;
+  std::cout << "The objective was to use OOP, SOLID principles and design patterns to develop a project." << std::endl;
+  std::cout << "Since CS246 is still being offered at the University of Waterloo, we cannot make the source code public due to academic policy." << std::endl;
+}
